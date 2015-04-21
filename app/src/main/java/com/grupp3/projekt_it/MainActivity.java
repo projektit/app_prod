15df73e6d610366d67fa1d5e514e6e8102c9e9fe
FeliciaSchnell <felicia.schnell@hotmail.com>
2015-04-21 11:34:33 +0200

    fixed hamburger problem and making back button in main activity exit app

--- app/src/main/java/com/grupp3/projekt_it/MainActivity.java
+++ app/src/main/java/com/grupp3/projekt_it/MainActivity.java
@@ -103,4 +103,12 @@ public class MainActivity extends BaseActivity {
                 break;
         }
     }
+    // on back button click, exit the application, hence skip the login screen
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
